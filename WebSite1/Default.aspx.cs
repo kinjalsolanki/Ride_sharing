5da7560f5c8a49416651d55efe38ff1bf7c523e4
kinjalsolanki <kinjalsolanki26@gmail.com>
2015-02-19 16:13:59 +0530

    Test

--- WebSite1/Default.aspx.cs
+++ WebSite1/Default.aspx.cs
@@ -13,7 +13,7 @@ public partial class _Default : System.Web.UI.Page
     }
     protected void Button1_Click(object sender, EventArgs e)
     {
-        localhost.Service mys = new localhost.Service();   // you need to create the object of the web service 
+        test.Service mys = new test.Service();   // you need to create the object of the web service 
         int a = Convert.ToInt32(TextBox1.Text);
         int b = Convert.ToInt32(TextBox2.Text);
         int c = mys.Multiplication(a, b);
