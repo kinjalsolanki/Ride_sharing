284814991ff94c648cdda5bf341e7b9d20841767
kinjalsolanki <kinjalsolanki26@gmail.com>
2015-02-25 10:57:48 +0530

    Draw routes between source and destn

--- RideShare/app/src/main/java/com/example/general/rideshare/MainActivity.java
+++ RideShare/app/src/main/java/com/example/general/rideshare/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.general.rideshare;
 
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -23,8 +24,18 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class MainActivity extends ActionBarActivity implements GoogleApiClient.ConnectionCallbacks,
@@ -34,9 +45,9 @@ public class MainActivity extends ActionBarActivity implements GoogleApiClient.C
     GoogleApiClient mGoogleApiClient;
     Location mLastLocation;
     TextView mLatitudeText, mLongitudeText;
-    Button source,dest;
+    Button source,dest,path;
     MarkerOptions markerOptions;
-    LatLng latLng;
+    LatLng SlatLng, DlatLng;
 
     double latitude=0,longitude=0;
     private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
@@ -46,12 +57,17 @@ public class MainActivity extends ActionBarActivity implements GoogleApiClient.C
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mLatitudeText =(TextView)findViewById(R.id.textView);
         mLongitudeText=(TextView)findViewById(R.id.textView1);
         source=(Button)findViewById(R.id.button);
         dest=(Button)findViewById(R.id.button1);
+        path=(Button) findViewById(R.id.button2);
+
         source.setOnClickListener(this);
         dest.setOnClickListener(this);
+        path.setOnClickListener(this);
+
         createMapView();
         googleMap.setMyLocationEnabled(true);
         if (checkPlayServices()) {
@@ -172,9 +188,9 @@ public class MainActivity extends ActionBarActivity implements GoogleApiClient.C
 
         switch(v.getId()) {
             case R.id.button:
-                latLng = new LatLng(latitude, longitude);
-                googleMap.addMarker(new MarkerOptions().position(latLng).title("You are here"));
-                googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                SlatLng = new LatLng(latitude, longitude);
+                googleMap.addMarker(new MarkerOptions().position(SlatLng).title("You are here"));
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(SlatLng));
                 googleMap.animateCamera(CameraUpdateFactory.zoomTo(15));
                 break;
             case R.id.button1:
@@ -183,9 +199,185 @@ public class MainActivity extends ActionBarActivity implements GoogleApiClient.C
                     new GeocoderTask().execute(location);
                 }
                 break;
+            case R.id.button2:
+                getPath();
         }
 
     }
+
+    private void getPath() {
+        String url = getDirectionsUrl(SlatLng, DlatLng);
+        DownloadTask downloadTask = new DownloadTask();
+
+        // Start downloading json data from Google Directions API
+        downloadTask.execute(url);
+    }
+
+
+    private String getDirectionsUrl(LatLng origin,LatLng dest){
+
+        // Origin of route
+        String str_origin = "origin="+origin.latitude+","+origin.longitude;
+
+        // Destination of route
+        String str_dest = "destination="+dest.latitude+","+dest.longitude;
+
+        // Sensor enabled
+        String sensor = "sensor=false";
+
+        // Waypoints
+        String waypoints = "";
+     /*   for(int i=2;i<markerPoints.size();i++){
+            LatLng point  = (LatLng) markerPoints.get(i);
+            if(i==2)
+                waypoints = "waypoints=";
+            waypoints += point.latitude + "," + point.longitude + "|";
+        }*/
+
+        // Building the parameters to the web service
+        String parameters = str_origin+"&"+str_dest+"&"+sensor+"&"+waypoints;
+
+        // Output format
+        String output = "json";
+
+        // Building the url to the web service
+        String url = "https://maps.googleapis.com/maps/api/directions/"+output+"?"+parameters;
+
+        return url;
+    }
+
+    private String downloadUrl(String strUrl) throws IOException{
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try{
+            URL url = new URL(strUrl);
+
+            // Creating an http connection to communicate with url
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            // Connecting to url
+            urlConnection.connect();
+
+            // Reading data from url
+            iStream = urlConnection.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+
+            StringBuffer sb  = new StringBuffer();
+
+            String line = "";
+            while( ( line = br.readLine())  != null){
+                sb.append(line);
+            }
+
+            data = sb.toString();
+
+            br.close();
+
+        }catch(Exception e){
+            Log.d("Exception while downloading url", e.toString());
+        }finally{
+            iStream.close();
+            urlConnection.disconnect();
+        }
+        return data;
+    }
+
+    private class DownloadTask extends AsyncTask<String, Void, String>{
+
+        // Downloading data in non-ui thread
+        @Override
+        protected String doInBackground(String... url) {
+
+            // For storing data from web service
+
+            String data = "";
+
+            try{
+                // Fetching the data from web service
+                data = downloadUrl(url[0]);
+            }catch(Exception e){
+                Log.d("Background Task",e.toString());
+            }
+            return data;
+        }
+
+
+        // Executes in UI thread, after the execution of
+        // doInBackground()
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            ParserTask parserTask = new ParserTask();
+
+            // Invokes the thread for parsing the JSON data
+            parserTask.execute(result);
+        }
+    }
+
+
+
+    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String,String>>> >{
+
+        // Parsing the data in non-ui thread
+        @Override
+        protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
+
+            JSONObject jObject;
+            List<List<HashMap<String, String>>> routes = null;
+
+            try{
+                jObject = new JSONObject(jsonData[0]);
+                DirectionsJSONParser parser = new DirectionsJSONParser();
+
+                // Starts parsing data
+                routes = parser.parse(jObject);
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            return routes;
+        }
+
+        // Executes in UI thread, after the parsing process
+        @Override
+        protected void onPostExecute(List<List<HashMap<String, String>>> result) {
+
+            ArrayList<LatLng> points = null;
+            PolylineOptions lineOptions = null;
+
+            // Traversing through all the routes
+            for(int i=0;i<result.size();i++){
+                points = new ArrayList<LatLng>();
+                lineOptions = new PolylineOptions();
+
+                // Fetching i-th route
+                List<HashMap<String, String>> path = result.get(i);
+
+                // Fetching all the points in i-th route
+                for(int j=0;j<path.size();j++){
+                    HashMap<String,String> point = path.get(j);
+
+                    double lat = Double.parseDouble(point.get("lat"));
+                    double lng = Double.parseDouble(point.get("lng"));
+                    LatLng position = new LatLng(lat, lng);
+
+                    points.add(position);
+                }
+
+                // Adding all the points in the route to LineOptions
+                lineOptions.addAll(points);
+                lineOptions.width(2);
+                lineOptions.color(Color.RED);
+            }
+
+            // Drawing polyline in the Google Map for the i-th route
+            googleMap.addPolyline(lineOptions);
+        }
+    }
+
+
 
     private class GeocoderTask extends AsyncTask<String, Void, List<Address>> {
 
@@ -220,18 +412,18 @@ public class MainActivity extends ActionBarActivity implements GoogleApiClient.C
                 Address address = (Address) addresses.get(i);
 
                 // Creating an instance of GeoPoint, to display in Google Map
-                latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                DlatLng = new LatLng(address.getLatitude(), address.getLongitude());
 
                 String addressText = String.format("%s, %s",
                         address.getMaxAddressLineIndex() > 0 ? address.getAddressLine(0) : "",
                         address.getCountryName());
 
-                googleMap.addMarker(new MarkerOptions().position(latLng).title(addressText));
+                googleMap.addMarker(new MarkerOptions().position(DlatLng).title(addressText));
 
                 // Locate the first location
                 if(i==0) {
                     //googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-                    googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                    googleMap.moveCamera(CameraUpdateFactory.newLatLng(DlatLng));
                     googleMap.animateCamera(CameraUpdateFactory.zoomTo(15));
                 }
             }
