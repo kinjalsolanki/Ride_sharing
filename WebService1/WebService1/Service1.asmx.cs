22fb43eebd2b8199cb4f3fd61bf6b8bacf140f34
kinjalsolanki <kinjalsolanki26@gmail.com>
2015-02-27 09:07:45 +0530

    web service

--- WebService1/WebService1/Service1.asmx.cs
+++ WebService1/WebService1/Service1.asmx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Services;
 using System.Data.SqlClient;
 using System.Data;
+using System.Configuration;
 
 [WebService(Namespace = "http://tempuri.org/")]
 [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
@@ -31,22 +32,24 @@ public class Service1 : System.Web.Services.WebService
         return (a * b);
     }
     [WebMethod]
-     public int createRoute(String User_id, String Source_id, String Dest_id, String Locations, int No_of_seats)
+     public String createRoute(String User_id, String Source_id, String Dest_id, String Locations, int No_of_seats)
      {
-         String connectionString = "Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\Users\\Boss\\Documents\\Visual Studio 2010\\Projects\\WebService1\\WebService1\\App_Data\\Database1.mdf;Integrated Security=True;User Instance=True;";
-         SqlConnection scon = new SqlConnection(connectionString);
+         //String connectionString = "Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\Users\\Boss\\Documents\\Visual Studio 2010\\Projects\\WebService1\\WebService1\\App_Data\\Database1.mdf;Integrated Security=True;User Instance=True;";
+         //SqlConnection scon = new SqlConnection(connectionString);
+         SqlConnection scon = new SqlConnection(ConfigurationManager.ConnectionStrings["RouteDatabase"].ConnectionString);
          scon.Open();
          String ss = "INSERT into route_info (user_id,source_id,dest_id,locations,no_of_seats) VALUES('" + User_id + "','" + Source_id + "','" + Dest_id + "','" + Locations + "','" + No_of_seats + "')";
          SqlCommand cmd1 = new SqlCommand(ss, scon);
          cmd1.ExecuteNonQuery();
          scon.Close();
-         return 0;
+         return "Route Created";
      }
     [WebMethod]
     public string returnRouteSD(String Source, String Dest)
     {
-        String connectionString = "Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\Users\\Boss\\Documents\\Visual Studio 2010\\Projects\\WebService1\\WebService1\\App_Data\\Database1.mdf;Integrated Security=True;User Instance=True;";
-        SqlConnection scon = new SqlConnection(connectionString);
+       // String connectionString = "Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\Users\\Boss\\Documents\\Visual Studio 2010\\Projects\\WebService1\\WebService1\\App_Data\\Database1.mdf;Integrated Security=True;User Instance=True;";
+        //SqlConnection scon = new SqlConnection(connectionString);
+        SqlConnection scon = new SqlConnection(ConfigurationManager.ConnectionStrings["RouteDatabase"].ConnectionString);
         scon.Open();
         String list = "select * from route_info where source_id='" + Source + "' and dest_id='" + Dest + "'";
         SqlCommand command = new SqlCommand(list, scon);
