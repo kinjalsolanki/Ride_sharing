47e5f39aecf9119d6a1bda1957503bd9ac56c10b
yugmamoradia <yugma93@gmail.com>
2015-03-06 14:53:02 +0530

    yesterday ka commit

--- WebService1/WebService1/Service1.asmx.cs
+++ WebService1/WebService1/Service1.asmx.cs
@@ -22,17 +22,19 @@ public class Service1 : System.Web.Services.WebService
     }
 
     [WebMethod]
-     public String createRoute(String User_id, String Source_id, String Dest_id, String Locations, int No_of_seats)
+     public String createRoute(String User_id, String Source_id, String Dest_id, String Locations, int No_of_seats, String Areas)
      {
          //String connectionString = "Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\Users\\Boss\\Documents\\Visual Studio 2010\\Projects\\WebService1\\WebService1\\App_Data\\Database1.mdf;Integrated Security=True;User Instance=True;";
          //SqlConnection scon = new SqlConnection(connectionString);
          SqlConnection scon = new SqlConnection(ConfigurationManager.ConnectionStrings["RouteDatabase"].ConnectionString);
          scon.Open();
-         String ss = "INSERT into route_info (user_id,source_id,dest_id,locations,no_of_seats) VALUES('" + User_id + "','" + Source_id + "','" + Dest_id + "','" + Locations + "','" + No_of_seats + "')";
+         String ss = "INSERT into route_info (user_id,source_id,dest_id,locations,no_of_seats,areas) VALUES('" + User_id + "','" + Source_id + "','" + Dest_id + "','" + Locations + "','" + No_of_seats + "','" + Areas + "')";
          SqlCommand cmd1 = new SqlCommand(ss, scon);
          cmd1.ExecuteNonQuery();
          scon.Close();
+         System.Diagnostics.Debug.WriteLine("-----------------Areas--------------"+Areas);
          return "Route Created";
+         
      }
     [WebMethod]
     public string returnRouteSD(String Source, String Dest)
@@ -40,11 +42,11 @@ public class Service1 : System.Web.Services.WebService
         String[] Sour = Source.Split(',');
         String[] Desti = Dest.Split(',');
 
-       // String connectionString = "Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\Users\\Boss\\Documents\\Visual Studio 2010\\Projects\\WebService1\\WebService1\\App_Data\\Database1.mdf;Integrated Security=True;User Instance=True;";
-        //SqlConnection scon = new SqlConnection(connectionString);
-        SqlConnection scon = new SqlConnection(ConfigurationManager.ConnectionStrings["RouteDatabase"].ConnectionString);
+        String connectionString = "Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\Users\\Boss\\Documents\\Visual Studio 2010\\Projects\\WebService1\\WebService1\\App_Data\\Database1.mdf;Integrated Security=True;User Instance=True;";
+        SqlConnection scon = new SqlConnection(connectionString);
+        //SqlConnection scon = new SqlConnection(ConfigurationManager.ConnectionStrings["RouteDatabase"].ConnectionString);
         scon.Open();
-        String list = "select * from route_info where source_id like'" + Sour[0] + "%," + Sour[1].Substring(0, Sour[1].Length - 1) + "%' and dest_id like'" + Desti[0] + "%," + Desti[1].Substring(0, Desti[1].Length - 1) + "%'";
+        /*String list = "select * from route_info where source_id like'" + Sour[0] + "%," + Sour[1].Substring(0, Sour[1].Length - 1) + "%' and dest_id like'" + Desti[0] + "%," + Desti[1].Substring(0, Desti[1].Length - 1) + "%'";
         SqlCommand command = new SqlCommand(list, scon);
         SqlDataReader DR = command.ExecuteReader();
         String loc = "";
@@ -76,9 +78,10 @@ public class Service1 : System.Web.Services.WebService
         {
             loc = loc + DR3["no_of_seats"].ToString() + DR3["locations"].ToString() + "|";
         }
-        DR3.Close();
+        DR3.Close();*/
+       // String list4 = "select * from route_info";
         scon.Close();
-        loc = loc.Substring(0,loc.Length-1);
-        return loc;
+        //loc = loc.Substring(0,loc.Length-1);
+        return null;
     }
 }
