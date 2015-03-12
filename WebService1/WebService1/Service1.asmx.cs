53e14cd41275733670944107d90914245b9adb15
kinjalsolanki <kinjalsolanki26@gmail.com>
2015-03-12 18:34:08 +0530

    notification done

--- WebService1/WebService1/Service1.asmx.cs
+++ WebService1/WebService1/Service1.asmx.cs
@@ -6,6 +6,9 @@ using System.Web.Services;
 using System.Data.SqlClient;
 using System.Data;
 using System.Configuration;
+using System.Net;
+using System.IO;
+using System.Text;
 
 [WebService(Namespace = "http://tempuri.org/")]
 [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
@@ -22,96 +25,104 @@ public class Service1 : System.Web.Services.WebService
     }
 
     [WebMethod]
-<<<<<<< HEAD
-     public String createRoute(String User_id, String Source_id, String Dest_id, String Locations, int No_of_seats, String Areas)
-=======
-    public String createRoute(String User_id, String Source_id, String Dest_id, String Locations, int No_of_seats, String Areas)
->>>>>>> origin/master
-     {
-         //String connectionString = "Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\Users\\Boss\\Documents\\Visual Studio 2010\\Projects\\WebService1\\WebService1\\App_Data\\Database1.mdf;Integrated Security=True;User Instance=True;";
-         //SqlConnection scon = new SqlConnection(connectionString);
-         SqlConnection scon = new SqlConnection(ConfigurationManager.ConnectionStrings["RouteDatabase"].ConnectionString);
-         scon.Open();
-         String ss = "INSERT into route_info (user_id,source_id,dest_id,locations,no_of_seats,areas) VALUES('" + User_id + "','" + Source_id + "','" + Dest_id + "','" + Locations + "','" + No_of_seats + "','" + Areas + "')";
-         SqlCommand cmd1 = new SqlCommand(ss, scon);
-         cmd1.ExecuteNonQuery();
-         System.Diagnostics.Debug.WriteLine("-----------------Areas--------------"+Areas);
-         scon.Close();
-         System.Diagnostics.Debug.WriteLine("-----------------Areas--------------"+Areas);
-         return "Route Created";
-         
-     }
-    [WebMethod]
-    public string returnRouteSD(String Areas)
+    public String createRoute(String User_id, String Source_id, String Dest_id, String Locations, int No_of_seats, String Areas, String Reg_id)
     {
-        //String[] Sour = Source.Split(',');
-        //String[] Desti = Dest.Split(',');
-
-<<<<<<< HEAD
-        String connectionString = "Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\Users\\Boss\\Documents\\Visual Studio 2010\\Projects\\WebService1\\WebService1\\App_Data\\Database1.mdf;Integrated Security=True;User Instance=True;";
-        SqlConnection scon = new SqlConnection(connectionString);
-        //SqlConnection scon = new SqlConnection(ConfigurationManager.ConnectionStrings["RouteDatabase"].ConnectionString);
-=======
         //String connectionString = "Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\Users\\Boss\\Documents\\Visual Studio 2010\\Projects\\WebService1\\WebService1\\App_Data\\Database1.mdf;Integrated Security=True;User Instance=True;";
         //SqlConnection scon = new SqlConnection(connectionString);
         SqlConnection scon = new SqlConnection(ConfigurationManager.ConnectionStrings["RouteDatabase"].ConnectionString);
->>>>>>> origin/master
         scon.Open();
-        /*String list = "select * from route_info where source_id like'" + Sour[0] + "%," + Sour[1].Substring(0, Sour[1].Length - 1) + "%' and dest_id like'" + Desti[0] + "%," + Desti[1].Substring(0, Desti[1].Length - 1) + "%'";
-        SqlCommand command = new SqlCommand(list, scon);
-        SqlDataReader DR = command.ExecuteReader();
-        String loc = "";
+        String ss = "INSERT into route_info (user_id,source_id,dest_id,locations,no_of_seats,areas,reg_id) VALUES('" + User_id + "','" + Source_id + "','" + Dest_id + "','" + Locations + "','" + No_of_seats + "','" + Areas + "','" + Reg_id + "')";
+        SqlCommand cmd1 = new SqlCommand(ss, scon);
+        cmd1.ExecuteNonQuery();
+        System.Diagnostics.Debug.WriteLine("-----------------Areas--------------" + Areas);
+        scon.Close();
+        return "Route Created";
+    }
+
+    public void SendNotification(string deviceId, string message)
+    {
+        string GoogleAppID = "AIzaSyDDEO1ceYkLYA0O309cPOZ27ceBky9KU-Q";
+        var SENDER_ID = "762985634560";
+        var value = message;
+        WebRequest tRequest;
+        tRequest = WebRequest.Create("https://android.googleapis.com/gcm/send");
+        tRequest.Method = "post";
+        tRequest.ContentType = " application/x-www-form-urlencoded;charset=UTF-8";
+        tRequest.Headers.Add(string.Format("Authorization: key={0}", GoogleAppID));
+
+        tRequest.Headers.Add(string.Format("Sender: id={0}", SENDER_ID));
+
+        string postData = "collapse_key=score_update&time_to_live=1000&delay_while_idle=0&data.message=" + value + "&data.time=" + System.DateTime.Now.ToString() + "&registration_id=" + deviceId + "";
+        Console.WriteLine(postData);
+        Byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+        tRequest.ContentLength = byteArray.Length;
+
+        Stream dataStream = tRequest.GetRequestStream();
+        dataStream.Write(byteArray, 0, byteArray.Length);
+        dataStream.Close();
+
+        WebResponse tResponse = tRequest.GetResponse();
+
+        dataStream = tResponse.GetResponseStream();
+
+        StreamReader tReader = new StreamReader(dataStream);
+
+        String sResponseFromServer = tReader.ReadToEnd();
+        System.Diagnostics.Debug.WriteLine(sResponseFromServer);
+
+
+        tReader.Close();
+        dataStream.Close();
+        tResponse.Close();
+
+    }
+
+    [WebMethod]
+    public String selectRoute(int RouteId, int Seats)
+    {
+        SqlConnection scon = new SqlConnection(ConfigurationManager.ConnectionStrings["RouteDatabase"].ConnectionString);
+        scon.Open();
+        String list = "select no_of_seats,reg_id from route_info where route_id=" + RouteId;
+        int seats = 3;
+        String reg_id = "";
+        SqlCommand command1 = new SqlCommand(list, scon);
+        SqlDataReader DR = command1.ExecuteReader();
         while (DR.Read())
         {
-            loc = DR["no_of_seats"].ToString()+DR["locations"].ToString()+"|";
+            seats = Int32.Parse(DR["no_of_seats"].ToString());
+            reg_id = DR["reg_id"].ToString();
         }
         DR.Close();
-        String list1 = "select * from route_info where locations like '%" + Sour[0] + "%," + Sour[1].Substring(0, Sour[1].Length - 1) + "%' and dest_id like'" + Desti[0] + "%," + Desti[1].Substring(0, Desti[1].Length - 1) + "%'";
-        SqlCommand command1 = new SqlCommand(list1, scon);
-        SqlDataReader DR1 = command1.ExecuteReader();
-        while (DR1.Read())
-        {
-            loc = loc + DR1["no_of_seats"].ToString() + DR1["locations"].ToString()+"|";
-        }
-        DR1.Close();
-        String list2 = "select * from route_info where locations like '%" + Sour[0] + "%," + Sour[1].Substring(0, Sour[1].Length - 1) + "%' and locations like'%" + Desti[0] + "%," + Desti[1].Substring(0, Desti[1].Length - 1) + "%'";
-        SqlCommand command2 = new SqlCommand(list2, scon);
-        SqlDataReader DR2 = command2.ExecuteReader();
-        while (DR2.Read())
-        {
-            loc = loc + DR2["no_of_seats"].ToString() + DR2["locations"].ToString()+"|";
-        }
-        DR2.Close();
-        String list3 = "select * from route_info where source_id like '%" + Sour[0] + "%," + Sour[1].Substring(0, Sour[1].Length - 1) + "%' and locations like'%" + Desti[0] + "%," + Desti[1].Substring(0, Desti[1].Length - 1) + "%'";
-        SqlCommand command3 = new SqlCommand(list3, scon);
-        SqlDataReader DR3 = command3.ExecuteReader();
-        while (DR3.Read())
-        {
-            loc = loc + DR3["no_of_seats"].ToString() + DR3["locations"].ToString() + "|";
-        }
-        DR3.Close();*/
-<<<<<<< HEAD
-       // String list4 = "select * from route_info";
-        scon.Close();
-        //loc = loc.Substring(0,loc.Length-1);
-        return null;
-=======
-        // String list4 = "select * from route_info";
+        seats = seats - Seats;
 
-        String list = "select * from route_info where areas like '%" + Areas + "%'";
+        SendNotification(reg_id, "test notification..........yooo");
+
+        list = "update route_info set no_of_seats=" + seats + "where route_id=" + RouteId;
+        command1 = new SqlCommand(list, scon);
+        command1.ExecuteNonQuery();
+
+        scon.Close();
+        return "Route Registered!";
+    }
+
+    [WebMethod]
+    public string returnRouteSD(String Areas, int Seats)
+    {
+        SqlConnection scon = new SqlConnection(ConfigurationManager.ConnectionStrings["RouteDatabase"].ConnectionString);
+        scon.Open();
+        String list = "select * from route_info where areas like '%" + Areas + "%' and no_of_seats >=" + Seats;
         String loc = "";
         SqlCommand command1 = new SqlCommand(list, scon);
         SqlDataReader DR1 = command1.ExecuteReader();
         while (DR1.Read())
         {
-            loc = loc + DR1["no_of_seats"].ToString() + DR1["locations"].ToString() + " ";
+            loc = loc + DR1["route_id"].ToString() + DR1["locations"].ToString() + " ";
         }
         DR1.Close();
         scon.Close();
         System.Diagnostics.Debug.WriteLine("-----------------Areas--------------" + loc);
-        
+
         //loc = loc.Substring(0,loc.Length-1);
         return loc;
->>>>>>> origin/master
     }
 }
